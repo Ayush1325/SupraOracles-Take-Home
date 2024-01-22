14155593e102115810abbb7536da88ba8ea5c4db
Ayush Singh <ayushdevel1325@gmail.com>
2024-01-22 17:44:42 +0530

    Cannot use Binance API
    - Seems the binance ban prevents me to use their API. Let's try bybit
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ struct Cli {
     #[clap(long)]
     mode: Mode,
     #[clap(long)]
-    times: u64
+    times: u64,
 }
 
 #[derive(ValueEnum, Clone, Debug)]
 enum Mode {
     Cache,
-    Read
+    Read,
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+
     tracing::info!("Hello, world!");
 }
