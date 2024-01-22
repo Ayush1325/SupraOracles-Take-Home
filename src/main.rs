c61d5f0fd716c6b6bfce18cc18af71eda11bbad6
Ayush Singh <ayushdevel1325@gmail.com>
2024-01-22 13:11:21 +0530

    Basic setup
    - Add deps
    - Use tokio and tracing
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(long)]
+    mode: Mode,
+    #[clap(long)]
+    times: u64
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum Mode {
+    Cache,
+    Read
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    tracing::info!("Hello, world!");
 }
