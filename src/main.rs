542d836d9cd4785dcbf3bd2724e71275f710e951
Ayush Singh <ayushdevel1325@gmail.com>
2024-01-22 23:56:38 +0530

    Task 1 complete
    - Maybe the code can look better but no point in hyper optimization
      until task 2 and 3 are done
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::{path::PathBuf, time::Duration};
 
 use clap::{Parser, ValueEnum};
-use futures_util::{pin_mut, SinkExt, StreamExt};
+use futures_util::{SinkExt, StreamExt};
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 use tokio::io::AsyncWriteExt;
+use tokio_tungstenite::tungstenite;
+
+const URL: &str = "wss://stream.bybit.com/v5/public/spot";
 
 #[derive(Parser)]
 struct Cli {
     #[clap(long)]
     mode: Mode,
-    #[clap(long)]
+    #[clap(long, default_value = "10")]
     times: u64,
     #[clap(default_value = "data.json")]
     file: PathBuf,
@@ -45,78 +48,109 @@ struct TickerData {
     usd_index_price: Decimal,
 }
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
+enum FileFormat {
+    DataPoint(Decimal),
+    Average(Decimal),
+}
+
+impl std::fmt::Display for FileFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FileFormat::DataPoint(x) => write!(f, "Data Point: {x}"),
+            FileFormat::Average(x) => write!(f, "Average: {x}"),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let (tx, mut rx) = tokio::sync::broadcast::channel::<TickerResponse>(10);
+    let cli = Cli::parse();
 
-    let (mut ws, resp) = tokio_tungstenite::connect_async("wss://stream.bybit.com/v5/public/spot")
-        .await
-        .expect("Failed to connect Websocket");
+    match cli.mode {
+        Mode::Cache => cache(cli.times, cli.file).await,
+        Mode::Read => tokio::task::spawn_blocking(|| read(cli.file))
+            .await
+            .expect("Failed to read"),
+    }
+}
+
+fn read(file: PathBuf) {
+    let file = std::fs::File::open(file).expect("Failed to open file");
+
+    serde_json::Deserializer::from_reader(file)
+        .into_iter::<FileFormat>()
+        .for_each(|x| {
+            let x = x.expect("Failed to deserialize");
+            println!("{x}");
+        });
+}
+
+async fn cache(time_in_sec: u64, file: PathBuf) {
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<TickerResponse>(10);
+
+    let compute_task = tokio::spawn(async move {
+        let mut file = tokio::fs::File::create(file)
+            .await
+            .expect("Failed to create file");
+        let mut avg = Decimal::new(0, 0);
+        let mut count = 0;
+
+        while let Some(msg) = rx.recv().await {
+            avg += msg.data.last_price;
+            count += 1;
+
+            let data = serde_json::to_vec(&FileFormat::DataPoint(msg.data.last_price)).unwrap();
+            file.write(&data).await.expect("Failed to write file");
+            file.write(b"\n").await.expect("Failed to write file");
+        }
+
+        avg /= Decimal::from(count);
+        println!("Cache Complete. The average USD price of BTC is: {avg}");
+
+        let data = serde_json::to_vec(&FileFormat::Average(avg)).unwrap();
+        file.write(&data).await.expect("Failed to write file");
+    });
+
+    let task = tokio::spawn(async move {
+        tracing::debug!("Start");
+
+        let ws = bybite_ws().await.expect("Failed to connect to bybite");
+        let tx = tokio_util::sync::PollSender::new(tx);
+        let _stream = ws
+            .filter_map(|msg| std::future::ready(msg.ok()))
+            .filter_map(|msg| async {
+                match msg {
+                    tungstenite::Message::Text(text) => {
+                        serde_json::from_str::<TickerResponse>(&text).ok()
+                    }
+                    _ => None,
+                }
+            })
+            .map(Ok)
+            .forward(tx)
+            .await;
+    });
+    tokio::time::sleep(Duration::from_secs(time_in_sec)).await;
+    task.abort();
+
+    compute_task.await.expect("Failed to compute");
+}
+
+async fn bybite_ws() -> anyhow::Result<
+    tokio_tungstenite::WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>,
+> {
+    let (mut ws, _resp) = tokio_tungstenite::connect_async(URL).await?;
 
     let req = SubscriptionRequest {
         op: "subscribe".to_string(),
         args: vec!["tickers.BTCUSDT".to_string()],
     };
 
-    ws.send(tokio_tungstenite::tungstenite::Message::Text(
-        serde_json::to_string(&req).expect("Failed to serialize request"),
-    ))
-    .await
-    .expect("Failed to send message");
+    ws.send(tungstenite::Message::Text(serde_json::to_string(&req)?))
+        .await?;
 
-    let mut rx1 = tx.subscribe();
-    let compute_task = tokio::spawn(async move {
-        let mut avg = Decimal::new(0, 0);
-        let mut count = 0;
-        while let Ok(msg) = rx1.recv().await {
-            avg += msg.data.last_price;
-            count += 1;
-            tracing::info!("Receive: {:?}", msg);
-        }
-
-        avg /= Decimal::from(count);
-        tracing::info!("The average USD price of BTC is: {avg}");
-    });
-
-    let file_task = tokio::spawn(async move {
-        let mut file = tokio::fs::File::create("data.json")
-            .await
-            .expect("Failed to create file");
-
-        while let Ok(msg) = rx.recv().await {
-            let data = serde_json::to_vec(&msg).expect("Failed to serialize response");
-            file.write(&data).await.expect("Failed to write file");
-            file.write(b"\n").await.expect("Failed to write file");
-        }
-    });
-
-    let task = tokio::spawn(async move {
-        tracing::info!("Start");
-
-        let stream = ws
-            .filter_map(|msg| async { msg.ok() })
-            .filter_map(|msg| async {
-                match msg {
-                    tokio_tungstenite::tungstenite::Message::Text(text) => {
-                        serde_json::from_str::<TickerResponse>(&text).ok()
-                    }
-                    _ => None,
-                }
-            });
-
-        pin_mut!(stream);
-        while let Some(x) = stream.next().await {
-            tx.send(x).expect("Failed to broadcast");
-        }
-    });
-
-    tokio::time::sleep(Duration::from_secs(10)).await;
-
-    task.abort();
-
-    let (first, second) = tokio::join!(file_task, compute_task);
-
-    tracing::info!("Finish");
+    Ok(ws)
 }
